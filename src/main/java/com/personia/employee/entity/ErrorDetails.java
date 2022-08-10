c498510f49d969873b0a99d055706c63a4f9764a
krutikawankhede <46954173+krutikawankhede@users.noreply.github.com>
2022-08-10 14:40:41 +0530

    Updated

---
+++ src/main/java/com/personia/employee/entity/ErrorDetails.java
@@ -0,0 +1,21 @@
+package com.personia.employee.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
